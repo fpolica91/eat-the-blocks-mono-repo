be69950a625930212e9e9801b904f0bcb76f5342
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-06-24 12:58:43 -0400

    R

--- oracles/migrations/1_initial_migration.js
+++ oracles/migrations/1_initial_migration.js
@@ -7,5 +7,4 @@ module.exports = async function (deployer, _network, addresses) {
   const oracle = await Oracle.deployed();
   await oracle.updateReporter(reporter, true);
   await deployer.deploy(Consumer, oracle.address);
-
 };
